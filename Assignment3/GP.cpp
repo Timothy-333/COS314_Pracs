dcda109e55f7849bb24c9c9653100871cd375416
Tim Tam <timothymdevilliers@gmail.com>
2024-05-25 14:55:51 +0200

    Worked On Assignment3

--- Assignment3/GP.cpp
+++ Assignment3/GP.cpp
@@ -6,150 +6,272 @@
 #include <utility>
 #include <sstream>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
-enum NodeType { FUNCTION, TERMINAL };
+enum NodeType
+{
+    FUNCTION,
+    TERMINAL
+};
 
-struct Node {
+struct Node
+{
     NodeType type;
-    double value; // Used if TERMINAL
-    char function; // Used if FUNCTION: '+', '-', '*', '/'
-    Node* left;
-    Node* right;
-    
+    double value;
+    char function;
+    Node *left;
+    Node *right;
+
     Node(NodeType t, double v = 0, char f = '\0') : type(t), value(v), function(f), left(nullptr), right(nullptr) {}
 };
 
-Node* generateRandomTree(int depth) {
-    if (depth == 0) {
-        return new Node(TERMINAL, rand() % 10); // Terminal node with random value
-    }
-    Node* node = new Node(FUNCTION, 0, "+-*/"[rand() % 4]);
-    node->left = generateRandomTree(depth - 1);
-    node->right = generateRandomTree(depth - 1);
-    return node;
-}
-vector<Node*> initializePopulation(int populationSize, int maxDepth) {
-    vector<Node*> population;
-    for (int i = 0; i < populationSize; ++i) {
-        population.push_back(generateRandomTree(maxDepth));
-    }
-    return population;
-}
-double evaluate(Node* node, const vector<double>& inputs) {
-    if (node->type == TERMINAL) {
+struct Metrics
+{
+    double accuracy;
+    double specificity;
+    double sensitivity;
+    double fMeasure;
+};
+
+double evaluate(Node *node, const vector<double> &inputs)
+{
+    if (node->type == TERMINAL)
+    {
         return node->value;
     }
     double leftValue = evaluate(node->left, inputs);
     double rightValue = evaluate(node->right, inputs);
-    switch (node->function) {
-        case '+': return leftValue + rightValue;
-        case '-': return leftValue - rightValue;
-        case '*': return leftValue * rightValue;
-        case '/': return rightValue != 0 ? leftValue / rightValue : 1;
-        default: return 0;
+    switch (node->function)
+    {
+    case '+':
+        return leftValue + rightValue;
+    case '-':
+        return leftValue - rightValue;
+    case '*':
+        return leftValue * rightValue;
+    case '/':
+        return rightValue != 0 ? leftValue / rightValue : 1;
+    default:
+        return 0;
     }
 }
-double fitness(Node* tree, const vector<vector<double>>& data, const vector<double>& outputs) {
+
+Metrics calculateMetrics(Node *bestTree, const vector<vector<double>> &testInputs, const vector<double> &testOutputs)
+{
+    int TP = 0, TN = 0, FP = 0, FN = 0;
+    for (size_t i = 0; i < testInputs.size(); ++i)
+    {
+        double predictedOutput = round(evaluate(bestTree, testInputs[i]));
+        double actualOutput = testOutputs[i];
+        if (predictedOutput == actualOutput)
+        {
+            if (predictedOutput == 1)
+            {
+                ++TP;
+            }
+            else
+            {
+                ++TN;
+            }
+        }
+        else
+        {
+            if (predictedOutput == 1)
+            {
+                ++FP;
+            }
+            else
+            {
+                ++FN;
+            }
+        }
+    }
+    double accuracy = static_cast<double>(TP + TN) / (TP + TN + FP + FN);
+    double specificity = static_cast<double>(TN) / (TN + FP);
+    double sensitivity = static_cast<double>(TP) / (TP + FN);
+    double fMeasure = 2 * (sensitivity * specificity) / (sensitivity + specificity);
+    return {accuracy, specificity, sensitivity, fMeasure};
+}
+
+Node *generateRandomTree(int depth)
+{
+    if (depth == 0)
+    {
+        return new Node(TERMINAL, rand() % 10);
+    }
+    Node *node = new Node(FUNCTION, 0, "+-*/"[rand() % 4]);
+    node->left = generateRandomTree(depth - 1);
+    node->right = generateRandomTree(depth - 1);
+    return node;
+}
+
+vector<Node *> initializePopulation(int populationSize, int maxDepth)
+{
+    vector<Node *> population;
+    for (int i = 0; i < populationSize; ++i)
+    {
+        population.push_back(generateRandomTree(maxDepth));
+    }
+    return population;
+}
+
+double fitness(Node *tree, const vector<vector<double>> &data, const vector<double> &outputs)
+{
     double totalError = 0;
-    for (size_t i = 0; i < data.size(); ++i) {
+    for (size_t i = 0; i < data.size(); ++i)
+    {
         double result = evaluate(tree, data[i]);
-        totalError += pow(result - outputs[i], 2); // Mean Squared Error
+        totalError += pow(result - outputs[i], 2);
     }
     return totalError / data.size();
 }
-Node* tournamentSelection(const vector<Node*>& population, const vector<double>& fitnesses) {
+
+Node *tournamentSelection(const vector<Node *> &population, const vector<double> &fitnesses)
+{
     int tournamentSize = 5;
-    Node* best = population[rand() % population.size()];
+    Node *best = population[rand() % population.size()];
     double bestFitness = fitnesses[0];
-    for (int i = 1; i < tournamentSize; ++i) {
-        Node* contender = population[rand() % population.size()];
+    for (int i = 1; i < tournamentSize; ++i)
+    {
+        Node *contender = population[rand() % population.size()];
         double contenderFitness = fitnesses[i];
-        if (contenderFitness < bestFitness) {
+        if (contenderFitness < bestFitness)
+        {
             best = contender;
             bestFitness = contenderFitness;
         }
     }
     return best;
 }
-Node* deepCopy(Node* node) {
-    if (node == nullptr) {
+
+Node *deepCopy(Node *node)
+{
+    if (node == nullptr)
+    {
         return nullptr;
     }
-    Node* newNode = new Node(node->type, node->value, node->function);
+    Node *newNode = new Node(node->type, node->value, node->function);
     newNode->left = deepCopy(node->left);
     newNode->right = deepCopy(node->right);
     return newNode;
 }
 
-Node* crossover(Node* parent1, Node* parent2) {
-    if (rand() % 2 == 0) {
+Node *crossover(Node *parent1, Node *parent2)
+{
+    if (rand() % 2 == 0)
+    {
         return deepCopy(parent1);
-    } else if (parent1->type == TERMINAL || parent2->type == TERMINAL) {
+    }
+    else if (parent1->type == TERMINAL || parent2->type == TERMINAL)
+    {
         return deepCopy(parent1->type == TERMINAL ? parent1 : parent2);
-    } else {
-        Node* node = new Node(FUNCTION, 0, parent2->function);
+    }
+    else
+    {
+        Node *node = new Node(FUNCTION, 0, parent2->function);
         node->left = crossover(parent1->left, parent2->left);
         node->right = crossover(parent1->right, parent2->right);
         return node;
     }
 }
 
-void mutate(Node* node, int maxDepth) {
-    if (node->type == FUNCTION && maxDepth > 0) {
-        if (rand() % 2 == 0) {
+void mutate(Node *node, int maxDepth)
+{
+    if (node->type == FUNCTION && maxDepth > 0)
+    {
+        if (rand() % 2 == 0)
+        {
             node->function = "+-*/"[rand() % 4];
         }
-        mutate(node->left, maxDepth - 1);
-        mutate(node->right, maxDepth - 1);
-    } else if (node->type == TERMINAL) {
-        node->value = rand() % 10;
+        else 
+        {
+            node->type = TERMINAL;
+            node->value = rand() % 10;
+        }
+    }
+    else if (node->type == TERMINAL && maxDepth > 0)
+    {
+        if (rand() % 2 == 0) // 50% chance to grow the tree
+        {
+            node->type = FUNCTION;
+            node->function = "+-*/"[rand() % 4];
+            node->left = generateRandomTree(maxDepth - 1);
+            node->right = generateRandomTree(maxDepth - 1);
+        }
+        else
+        {
+            node->value = rand() % 10;
+        }
     }
 }
 
-void evolve(vector<Node*>& population, const vector<vector<double>>& data, const vector<double>& outputs, int generations, double mutationRate) {
-    for (int g = 0; g < generations; ++g) {
+void evolve(vector<Node *> &population, const vector<vector<double>> &data, const vector<double> &outputs, int generations, double mutationRate)
+{
+    for (int g = 0; g < generations; ++g)
+    {
         vector<double> fitnesses;
-        for (Node* tree : population) {
+        for (Node *tree : population)
+        {
             fitnesses.push_back(fitness(tree, data, outputs));
         }
-        
-        vector<Node*> newPopulation;
-        for (size_t i = 0; i < population.size(); ++i) {
-            Node* parent1 = tournamentSelection(population, fitnesses);
-            Node* parent2 = tournamentSelection(population, fitnesses);
-            Node* child = crossover(parent1, parent2);
-            if (rand() / double(RAND_MAX) < mutationRate) {
+
+        vector<Node *> newPopulation;
+        for (size_t i = 0; i < population.size(); ++i)
+        {
+            Node *parent1 = tournamentSelection(population, fitnesses);
+            Node *parent2 = tournamentSelection(population, fitnesses);
+            Node *child = crossover(parent1, parent2);
+            if (rand() / double(RAND_MAX) < mutationRate)
+            {
                 mutate(child, 3);
             }
             newPopulation.push_back(child);
         }
-        
-        for (Node* tree : population) {
+
+        for (Node *tree : population)
+        {
             delete tree;
         }
         population = newPopulation;
+
+        // Calculate and print training accuracy after each generation
+        Node *bestTree = population[0];
+        double bestFitness = fitness(bestTree, data, outputs);
+        for (Node *tree : population)
+        {
+            double currentFitness = fitness(tree, data, outputs);
+            if (currentFitness < bestFitness)
+            {
+                bestTree = tree;
+                bestFitness = currentFitness;
+            }
+        }
+        Metrics metrics = calculateMetrics(bestTree, data, outputs);
+        cout << "Generation " << g + 1 << " Training Accuracy: " << metrics.accuracy * 100 << "%" << endl;
     }
 }
-vector<string> split(const string &s, char delimiter) 
+
+vector<string> split(const string &s, char delimiter)
 {
     vector<string> tokens;
     string token;
     istringstream tokenStream(s);
-    while (getline(tokenStream, token, delimiter)) 
+    while (getline(tokenStream, token, delimiter))
     {
         tokens.push_back(token);
     }
     return tokens;
 }
-pair<vector<vector<double>>, vector<double>> readData(const string &filename) 
+
+pair<vector<vector<double>>, vector<double>> readData(const string &filename)
 {
     ifstream file(filename);
     vector<vector<double>> inputs;
     vector<double> outputs;
 
-    if (!file.is_open()) 
+    if (!file.is_open())
     {
         cout << "Unable to open file" << endl;
         return make_pair(inputs, outputs);
@@ -157,11 +279,11 @@ pair<vector<vector<double>>, vector<double>> readData(const string &filename)
 
     string line;
     getline(file, line);
-    while (getline(file, line)) 
+    while (getline(file, line))
     {
         vector<string> tokens = split(line, ',');
         vector<double> input;
-        for (size_t i = 0; i < tokens.size() - 1; i++) 
+        for (size_t i = 0; i < tokens.size() - 1; i++)
         {
             input.push_back(stod(tokens[i]));
         }
@@ -173,14 +295,19 @@ pair<vector<vector<double>>, vector<double>> readData(const string &filename)
 
     return make_pair(inputs, outputs);
 }
-void printTree(Node* node) 
+
+void printTree(Node *node)
 {
-    if (node == nullptr) {
+    if (node == nullptr)
+    {
         return;
     }
-    if (node->type == TERMINAL) {
+    if (node->type == TERMINAL)
+    {
         cout << node->value;
-    } else {
+    }
+    else
+    {
         cout << "(" << node->function << " ";
         printTree(node->left);
         cout << " ";
@@ -188,23 +315,13 @@ void printTree(Node* node)
         cout << ")";
     }
 }
-double calculateAccuracy(Node* bestTree, const vector<vector<double>>& testInputs, const vector<double>& testOutputs) 
+
+int main()
 {
-    int correctPredictions = 0;
-    for (size_t i = 0; i < testInputs.size(); ++i) {
-        double predictedOutput = evaluate(bestTree, testInputs[i]);
-        if (round(predictedOutput) == testOutputs[i]) {
-            ++correctPredictions;
-        }
-    }
-    return (static_cast<double>(correctPredictions) / testInputs.size()) * 100;
-}
-int main() {
     cout << "Enter the seed: ";
     int seed;
     cin >> seed;
     srand(seed);
-
     pair<vector<vector<double>>, vector<double>> trainingData = readData("mushroom_train.csv");
     pair<vector<vector<double>>, vector<double>> testData = readData("mushroom_test.csv");
 
@@ -216,23 +333,32 @@ int main() {
     int populationSize = 100;
     int maxDepth = 6;
     int generations = 50;
-    double mutationRate = 0.1;
-
-    vector<Node*> population = initializePopulation(populationSize, maxDepth);
+    double mutationRate = 0.3;
+    auto start = chrono::high_resolution_clock::now();
+    vector<Node *> population = initializePopulation(populationSize, maxDepth);
 
     evolve(population, trainInputs, trainOutputs, generations, mutationRate);
 
-    Node* bestTree = population[0];
+    Node *bestTree = population[0];
     double bestFitness = fitness(bestTree, testInputs, testOutputs);
-    for (Node* tree : population) 
+    for (Node *tree : population)
     {
         double currentFitness = fitness(tree, testInputs, testOutputs);
-        if (currentFitness < bestFitness) {
+        if (currentFitness < bestFitness)
+        {
             bestTree = tree;
             bestFitness = currentFitness;
         }
     }
-    double accuracy = calculateAccuracy(bestTree, testInputs, testOutputs);
-    cout << "Accuracy on test data: " << accuracy << "%" << endl;
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "Best tree: ";
+    printTree(bestTree);
+    cout << endl << "Time taken: " << duration.count() << "ms" << endl;
+    Metrics metrics = calculateMetrics(bestTree, testInputs, testOutputs);
+    cout << "Accuracy: " << metrics.accuracy * 100 << "%" << endl;
+    cout << "Specificity: " << metrics.specificity << endl;
+    cout << "Sensitivity: " << metrics.sensitivity << endl;
+    cout << "F-measure: " << metrics.fMeasure << endl;
     return 0;
 }
--- Assignment3/NN.cpp
+++ Assignment3/NN.cpp
@@ -5,72 +5,59 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+
 using namespace std;
+
 class Neuron 
 {
-    public:
-        double error;
-        double value;
-        double bias;
-        vector<double> weights;
+public:
+    double error;
+    double value;
+    double bias;
+    vector<double> weights;
 };
 
 class Layer 
 {
-    public:
-        vector<Neuron> neurons;
+public:
+    vector<Neuron> neurons;
 };
 
 class NeuralNetwork 
 {
-    private:
-        double learningRate = 0.8;
-    public:
-        vector<Layer> layers;
-        double sigmoid(double x) 
+private:
+    double learningRate = 0.8;
+public:
+    vector<Layer> layers;
+    double sigmoid(double x) 
+    {
+        return 1.0 / (1.0 + exp(-x));
+    }
+    double sigmoidDerivative(double x) 
+    {
+        double sigmoidValue = sigmoid(x);
+        return sigmoidValue * (1 - sigmoidValue);
+    }
+    NeuralNetwork() 
+    {
+        Layer inputLayer;
+        for (int i = 0; i < 8; i++) 
         {
-            return 1.0 / (1.0 + exp(-x));
-        }
-        double sigmoidDerivative(double x) 
-        {
-            double sigmoidValue = sigmoid(x);
-            return sigmoidValue * (1 - sigmoidValue);
-        }
-        NeuralNetwork() 
-        {
-            Layer inputLayer;
-            for (int i = 0; i < 8; i++) 
-            {
-                Neuron neuron;
-                neuron.value = 0;
-                inputLayer.neurons.push_back(neuron);
-            }
-            layers.push_back(inputLayer);
-
-            Layer hiddenLayer;
-            double bias = 2.0 * rand() / RAND_MAX - 1;
-            for (int i = 0; i < 8; i++) 
-            {
-                Neuron neuron;
-                neuron.bias = bias;
-                neuron.value = 2.0 * rand() / RAND_MAX - 1;
-                for (int j = 0; j < 8; j++) 
-                {
-                    double weight = 2.0 * rand() / RAND_MAX - 1;
-                    while(weight == 0)
-                    {
-                        weight = 2.0 * rand() / RAND_MAX - 1;
-                    }
-                    neuron.weights.push_back(weight);
-                }
-                hiddenLayer.neurons.push_back(neuron);
-            }
-            layers.push_back(hiddenLayer);
-            Layer outputLayer;
             Neuron neuron;
+            neuron.value = 0;
+            inputLayer.neurons.push_back(neuron);
+        }
+        layers.push_back(inputLayer);
+
+        Layer hiddenLayer;
+        double bias = 2.0 * rand() / RAND_MAX - 1;
+        for (int i = 0; i < 8; i++) 
+        {
+            Neuron neuron;
+            neuron.bias = bias;
             neuron.value = 2.0 * rand() / RAND_MAX - 1;
-            neuron.bias = 2.0 * rand() / RAND_MAX - 1;
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < 8; j++) 
             {
                 double weight = 2.0 * rand() / RAND_MAX - 1;
                 while(weight == 0)
@@ -79,92 +66,117 @@ class NeuralNetwork
                 }
                 neuron.weights.push_back(weight);
             }
-            outputLayer.neurons.push_back(neuron);
-            layers.push_back(outputLayer);
+            hiddenLayer.neurons.push_back(neuron);
         }
-        int forwardPropagate(vector<double> input) 
+        layers.push_back(hiddenLayer);
+        Layer outputLayer;
+        Neuron neuron;
+        neuron.value = 2.0 * rand() / RAND_MAX - 1;
+        neuron.bias = 2.0 * rand() / RAND_MAX - 1;
+        for (int j = 0; j < 8; j++)
         {
-            for (size_t i = 0; i < layers.size(); i++) 
+            double weight = 2.0 * rand() / RAND_MAX - 1;
+            while(weight == 0)
             {
-                Layer &layer = layers[i];
-                for (size_t j = 0; j < layer.neurons.size(); j++) 
+                weight = 2.0 * rand() / RAND_MAX - 1;
+            }
+            neuron.weights.push_back(weight);
+        }
+        outputLayer.neurons.push_back(neuron);
+        layers.push_back(outputLayer);
+    }
+    int forwardPropagate(vector<double> input) 
+    {
+        for (size_t i = 0; i < layers.size(); i++) 
+        {
+            Layer &layer = layers[i];
+            for (size_t j = 0; j < layer.neurons.size(); j++) 
+            {
+                if (i == 0) 
                 {
-                    if (i == 0) 
+                    layer.neurons[j].value = input[j];
+                }
+                else
+                {
+                    Neuron &neuron = layer.neurons[j];
+                    double sum = 0;
+                    for (size_t k = 0; k < neuron.weights.size(); k++)
                     {
-                        layer.neurons[j].value = input[j];
+                        sum += neuron.weights[k] * layers[i - 1].neurons[k].value;
                     }
-                    else
-                    {
-                        Neuron &neuron = layer.neurons[j];
-                        double sum = 0;
-                        for (size_t k = 0; k < neuron.weights.size(); k++)
-                        {
-                            sum += neuron.weights[k] * layers[i - 1].neurons[k].value;
-                        }
-                        neuron.value = sigmoid(sum + neuron.bias);
-                    }
+                    neuron.value = sigmoid(sum + neuron.bias);
                 }
             }
-            double output = layers.back().neurons[0].value;
-            return output >= 0.5 ? 1 : 0;
         }
+        double output = layers.back().neurons[0].value;
+        return output >= 0.5 ? 1 : 0;
+    }
 
-        void backPropagate(vector<double> actualOutputs) 
+    void backPropagate(vector<double> actualOutputs) 
+    {
+        for (size_t i = 0; i < actualOutputs.size(); i++)
         {
-            for (size_t i = 0; i < actualOutputs.size(); i++)
+            Layer &outputLayer = layers.back();
+            Neuron &outputNeuron = outputLayer.neurons[i];
+            double outputError = actualOutputs[i] - outputNeuron.value;
+            outputNeuron.error = outputError * sigmoidDerivative(outputNeuron.value);
+            double newWeights[8];
+            for (int i = 0; i < 8; i++)
             {
-                Layer &outputLayer = layers.back();
-                Neuron &outputNeuron = outputLayer.neurons[i];
-                double outputError = actualOutputs[i] - outputNeuron.value;
-                outputNeuron.error = outputError * sigmoidDerivative(outputNeuron.value);
-                double newWeights[8];
-                for (int i = 0; i < 8; i++)
+                newWeights[i] = outputNeuron.weights[i] + learningRate * outputNeuron.error * layers[1].neurons[i].value;
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                layers[1].neurons[i].error = outputNeuron.error * outputNeuron.weights[i] * sigmoidDerivative(layers[1].neurons[i].value);
+                for (int j = 0; j < 8; j++)
                 {
-                    newWeights[i] = outputNeuron.weights[i] + learningRate * outputNeuron.error * layers[1].neurons[i].value;
-                }
-                for (int i = 0; i < 8; i++)
-                {
-                    layers[1].neurons[i].error = outputNeuron.error * outputNeuron.weights[i] * sigmoidDerivative(layers[1].neurons[i].value);
-                    for (int j = 0; j < 8; j++)
-                    {
-                        layers[1].neurons[i].weights[j] += learningRate * layers[1].neurons[i].error * layers[0].neurons[j].value;
-                    }
-                }
-                for (int i = 0; i < 8; i++)
-                {
-                    outputNeuron.weights[i] = newWeights[i];
+                    layers[1].neurons[i].weights[j] += learningRate * layers[1].neurons[i].error * layers[0].neurons[j].value;
                 }
             }
+            for (int i = 0; i < 8; i++)
+            {
+                outputNeuron.weights[i] = newWeights[i];
+            }
         }
+    }
 
+    void train(vector<vector<double>> inputs, vector<double> outputs, int epochs, double errorChangeThreshold = 0.0001) 
+    {
+        double previousMeanError = 0.0;
+        for (int epoch = 0; epoch < epochs; epoch++) 
+        {
+            double totalError = 0;
+            for (size_t i = 0; i < inputs.size(); i++) 
+            {
+                int prediction = forwardPropagate(inputs[i]);
+                backPropagate({outputs[i]});
+                double error = pow(outputs[i] - prediction, 2);
+                totalError += error;
+            }
+            double meanError = totalError / inputs.size();
+            cout << "Epoch: " << epoch + 1 << ", Error: " << meanError << endl;
 
-        void train(vector<vector<double>> inputs, vector<double> outputs, int epochs) 
-        {
-            for (int epoch = 0; epoch < epochs; epoch++) 
+            // Check if the change in mean error is less than the threshold
+            if (epoch > 0 && abs(previousMeanError - meanError) < errorChangeThreshold)
             {
-                double totalError = 0;
-                for (size_t i = 0; i < inputs.size(); i++) 
-                {
-                    int prediction = forwardPropagate(inputs[i]);
-                    backPropagate({outputs[i]});
-                    double error = pow(outputs[i] - prediction, 2);
-                    totalError += error;
-                }
-                double meanError = totalError / inputs.size();
-                cout << "Epoch: " << epoch + 1 << ", Error: " << meanError << endl;
+                cout << "Stopping training, change in error less than threshold." << endl;
+                break;
             }
+            previousMeanError = meanError;
         }
-        vector<int> predict(vector<vector<double>> inputs) 
+    }
+
+    vector<int> predict(vector<vector<double>> inputs) 
+    {
+        vector<int> predictions;
+        for (size_t i = 0; i < inputs.size(); i++)
         {
-            vector<int> predictions;
-            for (size_t i = 0; i < inputs.size(); i++)
-            {
-                predictions.push_back(forwardPropagate(inputs[i]));
-                
-            }
-            return predictions;
+            predictions.push_back(forwardPropagate(inputs[i]));
         }
+        return predictions;
+    }
 };
+
 vector<string> split(const string &s, char delimiter) 
 {
     vector<string> tokens;
@@ -176,6 +188,7 @@ vector<string> split(const string &s, char delimiter)
     }
     return tokens;
 }
+
 pair<vector<vector<double>>, vector<double>> readData(const string &filename) 
 {
     ifstream file(filename);
@@ -206,6 +219,43 @@ pair<vector<vector<double>>, vector<double>> readData(const string &filename)
 
     return make_pair(inputs, outputs);
 }
+
+struct Metrics 
+{
+    double accuracy;
+    double specificity;
+    double sensitivity;
+    double fMeasure;
+};
+
+Metrics calculateMetrics(NeuralNetwork& nn, const vector<vector<double>>& testInputs, const vector<double>& testOutputs) 
+{
+    int TP = 0, TN = 0, FP = 0, FN = 0;
+    vector<int> predictions = nn.predict(testInputs);
+    for (size_t i = 0; i < testInputs.size(); ++i) {
+        int predictedOutput = predictions[i];
+        int actualOutput = testOutputs[i];
+        if (predictedOutput == actualOutput) {
+            if (predictedOutput == 1) {
+                ++TP;
+            } else {
+                ++TN;
+            }
+        } else {
+            if (predictedOutput == 1) {
+                ++FP;
+            } else {
+                ++FN;
+            }
+        }
+    }
+    double accuracy = static_cast<double>(TP + TN) / (TP + TN + FP + FN);
+    double specificity = static_cast<double>(TN) / (TN + FP);
+    double sensitivity = static_cast<double>(TP) / (TP + FN);
+    double fMeasure = 2 * (sensitivity * specificity) / (sensitivity + specificity);
+    return {accuracy, specificity, sensitivity, fMeasure};
+}
+
 int main() 
 {
     cout << "Enter the seed: ";
@@ -214,22 +264,21 @@ int main()
     srand(seed);
     pair<vector<vector<double>>, vector<double>> trainingData = readData("mushroom_train.csv");
     pair<vector<vector<double>>, vector<double>> testData = readData("mushroom_test.csv");
-
+    auto start = chrono::high_resolution_clock::now();
     NeuralNetwork nn;
     int epochs = 50;
     nn.train(trainingData.first, trainingData.second, epochs);
 
     vector<int> predictions = nn.predict(testData.first);
 
-    int correct = 0;
-    for (size_t i = 0; i < predictions.size(); i++)
-    {
-        if (predictions[i] == testData.second[i]) {
-            correct++;
-        }
-    }
-    double accuracy = 100.0 * correct / predictions.size();
-    cout << "Accuracy: " << accuracy << "%" << endl;
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "Time taken: " << duration.count() << "ms" << endl;
+    Metrics metrics = calculateMetrics(nn, testData.first, testData.second);
+    cout << "Accuracy: " << metrics.accuracy * 100 << "%" << endl;
+    cout << "Specificity: " << metrics.specificity << endl;
+    cout << "Sensitivity: " << metrics.sensitivity << endl;
+    cout << "F-measure: " << metrics.fMeasure << endl;
 
     return 0;
 }
